2c41920886797097559c23da14cc2cf9776e801e
Julio Alcantara <jalcantara88@gmail.com>
2021-06-23 16:17:17 -0700

    restructured the components to make them more effiecient and easier to read

---
+++ components/CustomView.js
@@ -0,0 +1,131 @@
+import React from 'react';
+import {Linking} from 'react-native';
+import {FlatList, Image, View, Text, StyleSheet} from 'react-native';
+
+const CustomView = ({name, image_url, issue, link, year}) => {
+    return(
+        <>
+            {/*basic card view*/}
+            <View style={styles.resultHolder} >
+                <Image source={{uri: image_url}} style={styles.image}/>
+                {/*Info Display*/}
+                <View style={styles.containerText}>
+                    {/*Name*/}
+                    <View style={styles.labelGroup}>
+                        <Text style={styles.label}>name: </Text>
+                        <Text style={styles.detail}>
+                            {name}
+                        </Text>
+                    </View>
+                    {/*Issue Number*/}
+                    <View style={styles.labelGroup}>
+                        <Text style={styles.label}>issue #: </Text>
+                        <Text style={styles.detail}>
+                            {issue}
+                        </Text>
+                    </View>
+                    {/*Year Released*/}
+                    <View style={styles.labelGroup}>
+                        <Text style={styles.label}>year: </Text>
+                        <Text style={styles.detail}>
+                            {year}
+                        </Text>
+                    </View>
+                    {/*open link to comicVine website showcasing this issue*/}
+                    <Text style={styles.linkButton} onPress={() => Linking.openURL(link)}>
+                        MORE DETAILS
+                    </Text>
+                </View>
+            </View>
+        </>
+    )
+}
+
+{/*Custom list view that uses flatlist to iterate over array holding api response*/}
+const CustomListView = ({itemList, toggleModal, modalVisible, setImage}) => {
+    return(
+    <View style={styles.issuesHolder}>
+        <FlatList
+            keyExtractor={item => item.id.toString()}
+            data={itemList}
+            
+            initialNumToRender={5}
+            renderItem={({item}) => {
+                return(
+                <CustomView
+                    name={item.volume.name}
+                    image_url={item.image.original_url}
+                    issue={item.issue_number}
+                    link={item.site_detail_url}
+                    year={item.cover_date ? item.cover_date : "N/A"}
+                    toggleModal={toggleModal}
+                    modalVisible={modalVisible}
+                    setImage={setImage}
+                />
+                )
+            }}
+        />
+    </View>
+    )}
+const styles = StyleSheet.create({
+    resultHolder: {
+        flex: 1,
+        flexDirection: 'row',
+        padding: 15,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 1 },
+        shadowOpacity: 0.8,
+        shadowRadius: 2,  
+        elevation: 5,
+        backgroundColor: '#fff',
+        margin: 5,
+        justifyContent: 'center',
+        borderRadius: 10
+    },
+    issuesHolder: {
+        flex: 1
+    },
+    detail: {
+        fontSize: 25,
+        fontWeight: 500,
+        padding: 5,
+        textAlign: 'center'
+    },
+    containerText: {
+        flex: 1,
+        flexDirection: 'column',
+        marginLeft: 22,
+        justifyContent: 'center'
+    },
+    linkButton: {
+        textDecorationLine: 'none',
+        backgroundColor: 'dodgerblue',
+        paddingVertical: 5,
+        textAlign: 'center',
+        fontSize: 20,
+        fontWeight: 400,
+        borderRadius: 5,
+        color: 'white'
+    },
+    image: {
+        width: '200px',
+        height: '300px',
+        resizeMode: 'cover',
+        
+    },
+    label: {
+        backgroundColor: 'crimson',
+        color: 'white',
+        padding: 3,
+        paddingLeft: 5,
+        borderTopLeftRadius: 5,
+        borderTopRightRadius: 5,
+    },
+    labelGroup: {
+        backgroundColor: 'lightpink',
+        borderRadius: 5,
+        marginBottom: 5,
+    },
+    });
+
+export default CustomListView;
--- components/MainComponent.js
+++ components/MainComponent.js
@@ -2,79 +2,13 @@ import React, { Component } from 'react';
 import { 
     StyleSheet, 
     Text, 
-    View, 
-    FlatList, 
-    Image, 
+    View,  
     Button, 
 } from 'react-native';
-import {Linking} from 'react-native';
+//animated svg for when loading
 import Spinner from '../assets/spinner.svg';
-
-const CustomView = ({name, image_url, issue, link, year}) => {
-    return(
-        <>
-            {/*basic card view*/}
-            <View style={styles.resultHolder} >
-                <Image source={{uri: image_url}} style={styles.image}/>
-                {/*Info Display*/}
-                <View style={styles.containerText}>
-                    {/*Name*/}
-                    <View style={styles.labelGroup}>
-                        <Text style={styles.label}>name: </Text>
-                        <Text style={styles.detail}>
-                            {name}
-                        </Text>
-                    </View>
-                    {/*Issue Number*/}
-                    <View style={styles.labelGroup}>
-                        <Text style={styles.label}>issue #: </Text>
-                        <Text style={styles.detail}>
-                            {issue}
-                        </Text>
-                    </View>
-                    {/*Year Released*/}
-                    <View style={styles.labelGroup}>
-                        <Text style={styles.label}>year: </Text>
-                        <Text style={styles.detail}>
-                            {year}
-                        </Text>
-                    </View>
-                    {/*open link to comicVine website showcasing this issue*/}
-                    <Text style={styles.linkButton} onPress={() => Linking.openURL(link)}>
-                        MORE DETAILS
-                    </Text>
-                </View>
-            </View>
-        </>
-    )
-}
-
-{/*Custom list view that uses flatlist to iterate over array holding api response*/}
-const CustomListView = ({itemList, toggleModal, modalVisible, setImage}) => {
-    return(
-    <View style={styles.issuesHolder}>
-        <FlatList
-            keyExtractor={item => item.id.toString()}
-            data={itemList}
-            
-            initialNumToRender={5}
-            renderItem={({item}) => {
-                return(
-                <CustomView
-                    name={item.volume.name}
-                    image_url={item.image.original_url}
-                    issue={item.issue_number}
-                    link={item.site_detail_url}
-                    year={item.cover_date ? item.cover_date : "N/A"}
-                    toggleModal={toggleModal}
-                    modalVisible={modalVisible}
-                    setImage={setImage}
-                />
-                )
-            }}
-        />
-    </View>
-    )}
+//custom render view for displaying api results
+import CustomListView from './CustomView';
 
 //create class to hold state and render list
 class Main extends Component {
@@ -92,11 +26,6 @@ class Main extends Component {
             //holds offset for api call
             apiOffset: 0
         }
-    }
-
-    toggleModal = () => {
-        this.setState({modalVisible: !this.state.modalVisible});
-        console.log(this.state.modalVisible);
     }
 
     apiCall(offset) {
@@ -159,7 +88,6 @@ class Main extends Component {
                         onPress={() => {
                             //set new ofset value then pass that into api call function on second param
                             var newNum = this.state.apiOffset + 100;
-                            
                             this.setState({apiOffset: newNum}, () => {
                                 this.setState({isLoading: true});
                                 this.apiCall(this.state.apiOffset);
@@ -183,64 +111,12 @@ class Main extends Component {
 }
 //Stylesheet for page
 const styles = StyleSheet.create({
-    resultHolder: {
-        flex: 1,
-        flexDirection: 'row',
-        padding: 15,
-        shadowColor: '#000',
-        shadowOffset: { width: 0, height: 1 },
-        shadowOpacity: 0.8,
-        shadowRadius: 2,  
-        elevation: 5,
-        backgroundColor: '#fff',
-        margin: 5,
-        justifyContent: 'center',
-        borderRadius: 10
-    },
-    issuesHolder: {
-        flex: 1
-    },
     detail: {
         fontSize: 25,
         fontWeight: 500,
         padding: 5,
         textAlign: 'center'
-    },
-    containerText: {
-        flex: 1,
-        flexDirection: 'column',
-        marginLeft: 22,
-        justifyContent: 'center'
-    },
-    linkButton: {
-        textDecorationLine: 'none',
-        backgroundColor: 'dodgerblue',
-        paddingVertical: 5,
-        textAlign: 'center',
-        fontSize: 20,
-        fontWeight: 400,
-        borderRadius: 5,
-        color: 'white'
-    },
-    image: {
-        width: '200px',
-        height: '300px',
-        resizeMode: 'cover',
-        
-    },
-    label: {
-        backgroundColor: 'crimson',
-        color: 'white',
-        padding: 3,
-        paddingLeft: 5,
-        borderTopLeftRadius: 5,
-        borderTopRightRadius: 5,
-    },
-    labelGroup: {
-        backgroundColor: 'lightpink',
-        borderRadius: 5,
-        marginBottom: 5,
-    },
+    }
   });
   
 export default Main;
